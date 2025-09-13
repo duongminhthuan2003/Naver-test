d92b92b9d9e945740172632d5b80ece18cb25b36
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-09-14 00:32:00 +0700

    Your changes 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,23 @@ import HomePage from './pages/HomePage.tsx'
 import SettingsPage from './pages/SettingsPage.tsx'
 import TasksPage from './pages/TasksPage.tsx'
 import NavigationBar from "./components/NavigationBar.tsx";
-import { AnimatePresence } from "framer-motion";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import SearchPage from "./pages/SearchPage.tsx";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="m-4">
         <NavigationBar />
 
         <div>
-            <AnimatePresence>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/tasks" element={<TasksPage />} />
-                    <Route path="/settings" element={<SettingsPage />} />
-                    <Route path="/search" element={<SearchPage />} />
-                </Routes>
-            </AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/tasks" element={<TasksPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/search" element={<SearchPage />} />
+            </Routes>
         </div>
     </div>
   )
